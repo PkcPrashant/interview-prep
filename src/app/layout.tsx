f843e0f6382e222b4d5818bce3b157bec2d81ee2
Prashant Choudhary <pc22230@gmail.com>
2025-07-10 13:16:20 +0530

    basic ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
         className={`${monaSans.className} antialiased pattern`}
       >
         {children}
-
         <Toaster />
       </body>
     </html>
